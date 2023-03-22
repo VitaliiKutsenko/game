fe247222f9e7e4c5482c6d818bbef4bd0354487a
vitalii89 <vitalii.kutsenko89@gmail.com>
2023-03-22 17:09:08 +0300

    push to git

--- src/routes/rootRouter.js
+++ src/routes/rootRouter.js
@@ -12,7 +12,7 @@ import { Fight } from '../pages/fight/fight';
 import { Private } from './privateRoutes';
 import { CharacterCreator } from '../components/characterCreator/characterCreator';
 
-export const rootRouter = createBrowserRouter(
+export const rootRouter = createHashRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
       <Route
